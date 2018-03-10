f94fc4c5664a819692268c96c0c5cc5f49a6ff47
claudia-romano <claudia.romano06@gmail.com>
2018-03-10 14:57:54 +0000

    Remove bourbon dependency
    - Remove bourbon dependency
    - Improvements to css/js

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ A handy tabbed navigation, optimized for mobile devices, with support for both h
 [Demo](http://codyhouse.co/demo/responsive-tabbed-navigation/index.html)
  
 [Terms](http://codyhouse.co/terms/)
+
+Icons: [Nucleo](https://nucleoapp.com)
--- js/main.js
+++ js/main.js
@@ -1,52 +1,123 @@
-jQuery(document).ready(function($){
-	var tabs = $('.cd-tabs');
-	
-	tabs.each(function(){
-		var tab = $(this),
-			tabItems = tab.find('ul.cd-tabs-navigation'),
-			tabContentWrapper = tab.children('ul.cd-tabs-content'),
-			tabNavigation = tab.find('nav');
+(function(){
+	function TabbedNavigation( element ) {
+		this.element = element;
+		this.navigation = this.element.getElementsByTagName("nav")[0];
+		this.navigationElements = this.navigation.getElementsByClassName("js-cd-navigation")[0];
+		this.content = this.element.getElementsByClassName("js-cd-content")[0];
+		this.activeTab;
+		this.activeContent;
+		this.init();
+	};
 
-		tabItems.on('click', 'a', function(event){
+	TabbedNavigation.prototype.init = function() {
+		var self = this;
+		//listen for the click on the tabs navigation
+		this.navigation.addEventListener("click", function(event){
 			event.preventDefault();
-			var selectedItem = $(this);
-			if( !selectedItem.hasClass('selected') ) {
-				var selectedTab = selectedItem.data('content'),
-					selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]'),
-					slectedContentHeight = selectedContent.innerHeight();
-				
-				tabItems.find('a.selected').removeClass('selected');
-				selectedItem.addClass('selected');
-				selectedContent.addClass('selected').siblings('li').removeClass('selected');
-				//animate tabContentWrapper height when content changes 
-				tabContentWrapper.animate({
-					'height': slectedContentHeight
-				}, 200);
+			if(event.target.tagName.toLowerCase() == "a" && !hasClass(event.target, "cd-selected")) {
+				self.activeTab = event.target;
+				self.activeContent = self.content.querySelectorAll("[data-content="+self.activeTab.getAttribute("data-content")+"]")[0];
+				self.updateContent();
 			}
 		});
 
-		//hide the .cd-tabs::after element when tabbed navigation has scrolled to the end (mobile version)
-		checkScrolling(tabNavigation);
-		tabNavigation.on('scroll', function(){ 
-			checkScrolling($(this));
+		//listen for the scroll in the tabs navigation 
+		this.navigation.addEventListener('scroll', function(event){
+			self.toggleNavShadow();
 		});
-	});
-	
-	$(window).on('resize', function(){
-		tabs.each(function(){
-			var tab = $(this);
-			checkScrolling(tab.find('nav'));
-			tab.find('.cd-tabs-content').css('height', 'auto');
-		});
-	});
+	};
 
-	function checkScrolling(tabs){
-		var totalTabWidth = parseInt(tabs.children('.cd-tabs-navigation').width()),
-		 	tabsViewport = parseInt(tabs.width());
-		if( tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
-			tabs.parent('.cd-tabs').addClass('is-ended');
-		} else {
-			tabs.parent('.cd-tabs').removeClass('is-ended');
+	TabbedNavigation.prototype.updateContent = function() {
+		var actualHeight = this.content.offsetHeight;
+		//update navigation classes
+		removeClass(this.navigation.querySelectorAll("a.cd-selected")[0], "cd-selected");
+		addClass(this.activeTab, "cd-selected");
+		//update content classes
+		removeClass(this.content.querySelectorAll("li.cd-selected")[0], "cd-selected");
+		addClass(this.activeContent, "cd-selected");
+		//set new height for the content wrapper
+		(!window.requestAnimationFrame) 
+			? this.content.setAttribute("style", "height:"+this.activeContent.offsetHeight+"px;")
+			: setHeight(actualHeight, this.activeContent.offsetHeight, this.content, 200);
+	};
+
+	TabbedNavigation.prototype.toggleNavShadow = function() {
+		//show/hide tabs navigation gradient layer
+		this.content.removeAttribute("style");
+		var navigationWidth = Math.floor(this.navigationElements.getBoundingClientRect().width),
+			navigationViewport = Math.ceil(this.navigation.getBoundingClientRect().width);
+		( this.navigation.scrollLeft >= navigationWidth - navigationViewport )
+			? addClass(this.element, "cd-tabs--scroll-ended")
+			: removeClass(this.element, "cd-tabs--scroll-ended");
+	};
+
+	var tabs = document.getElementsByClassName("js-cd-tabs"),
+		tabsArray = [],
+		resizing = false;
+	if( tabs.length > 0 ) {
+		for( var i = 0; i < tabs.length; i++) {
+			(function(i){
+				tabsArray.push(new TabbedNavigation(tabs[i]));
+			})(i);
 		}
+
+		window.addEventListener("resize", function(event) {
+			if( !resizing ) {
+				resizing = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkTabs, 250) : window.requestAnimationFrame(checkTabs);
+			}
+		});
 	}
-});
+
+	function checkTabs() {
+		tabsArray.forEach(function(tab){
+			tab.toggleNavShadow();
+		});
+		resizing = false;
+	};
+
+	function setHeight(start, to, element, duration) {
+		var change = to - start,
+	        currentTime = null;
+	        
+	    var animateHeight = function(timestamp){  
+	    	if (!currentTime) currentTime = timestamp;         
+	        var progress = timestamp - currentTime;
+	        var val = Math.easeInOutQuad(progress, start, change, duration);
+	        element.setAttribute("style", "height:"+val+"px;");
+	        if(progress < duration) {
+	            window.requestAnimationFrame(animateHeight);
+	        }
+	    };
+	    
+	    window.requestAnimationFrame(animateHeight);
+	}
+
+	Math.easeInOutQuad = function (t, b, c, d) {
+ 		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
+		t--;
+		return -c/2 * (t*(t-2) - 1) + b;
+	};
+	
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+})();
